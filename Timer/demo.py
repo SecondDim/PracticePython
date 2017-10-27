5f6b53a8f694534d665af6d97b4162df38927fcb
SecondDim <SecondDim@users.noreply.github.com>
2017-10-27 11:04:09 +0800

    update demo
    process_time vs perf_counter

--- Timer/demo.py
+++ Timer/demo.py
@@ -3,17 +3,26 @@
 import time
 
 
+def loop():
+    i = 100000
+    while i:
+        i = i - 1
+
+
 def run():
     '''
     process_time <include> cpu sleep time
+    > Return the value (in fractional seconds) of the sum of the system and user CPU time of the current process.
+    > (sum of mulit core process time)
     > It does include time elapsed during sleep and is system-wide.
 
     perf_counter <not include> cpu sleep time
+    > Return the value (in fractional seconds) of a performance counter
     > It does not include time elapsed during sleep.
     '''
 
     pro_st = time.process_time()
-    time.sleep(1)
+    loop()
     pro_ed = time.process_time()
 
     per_st = time.perf_counter()
@@ -22,11 +31,14 @@ def run():
 
     print('run time use process_time: ' + str(pro_ed - pro_st) + ' s')
     print('run time use perf_counter: ' + str(per_ed - per_st) + ' s')
-    print('pro_st: ' + str(pro_st))
-    print('pro_ed: ' + str(pro_ed))
-    print('per_st: ' + str(per_st))
-    print('per_ed: ' + str(per_ed))
+
+    print('process_time_st: ' + str(pro_st))
+    print('process_time_ed: ' + str(pro_ed))
+    print('perf_counter_st: ' + str(per_st))
+    print('perf_counter_ed: ' + str(per_ed))
     print('--------------------------------------------------')
+
+    time.sleep(5)
 
 
 if __name__ == '__main__':
