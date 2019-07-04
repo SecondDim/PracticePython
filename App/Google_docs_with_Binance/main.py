5fab0c34a7642ee25c63f515df24248ad581658c
SecondDim <SecondDim@users.noreply.github.com>
2019-07-04 10:59:04 +0800

    add bitorpo exchange

--- App/Google_docs_with_Binance/main.py
+++ App/Google_docs_with_Binance/main.py
@@ -8,12 +8,14 @@
 # Other way use https://github.com/david-pettifor-nd/easy-google-docs
 
 # binance https://github.com/binance-exchange/python-binance
+# bitopro https://developer.bitopro.com/docs
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import time
 import os
 import json
+import requests
 
 from binance.client import Client
 
@@ -39,6 +41,8 @@ def update_sheet(service, spreadsheet_id, update_data):
     update_at = time.strftime("%Y-%m-%d %H:%M:%S")
     worksheet.update_acell('B1', update_at)
 
+    print('Last update_at: %s' % update_at, end='\r')
+
 
 def binance_client(binance_api_key='ENV/binance-api-key.json'):
     if not os.path.exists(binance_api_key):
@@ -48,6 +52,19 @@ def binance_client(binance_api_key='ENV/binance-api-key.json'):
         binance = json.load(f)
 
     return Client(binance['api_key'], binance['api_secret'])
+
+
+def bitopro_open_api(request_method=None, request_params=None, request_url='https://api.bitopro.com/v2'):
+    if request_method == None or request_params == None:
+        raise "Missing args. func <<bitopro_open_api>>"
+
+    request_url = "%s/%s/%s" % (request_url, request_method, request_params)
+    r = requests.get(request_url)
+
+    if not r.ok:
+        raise "Api request fail. func <<bitopro_open_api>>"
+
+    return r.json()
 
 
 def gspread_service(service_account_key='ENV/service-account-key.json'):
@@ -66,14 +83,33 @@ def main():
 
     update_data = []
 
+    # Binance
     client = binance_client()
     all_tickers = client.get_all_tickers()
     for ticker in all_tickers:
         if ticker['symbol'] in MARKETS_SYMBOL:
             update_data.append(ticker)
 
+    # Bitopro
+    bitopro_response = bitopro_open_api(
+        request_method='tickers', request_params='btc_twd')
+    update_data.append({'symbol': 'BTCTWD',
+                        'price': bitopro_response['data']['lastPrice']})
+
+    bitopro_response = bitopro_open_api(
+        request_method='tickers', request_params='eth_twd')
+    update_data.append({'symbol': 'ETHTWD',
+                        'price': bitopro_response['data']['lastPrice']})
+
+    bitopro_response = bitopro_open_api(
+        request_method='tickers', request_params='usdt_twd')
+    update_data.append({'symbol': 'USDTTWD',
+                        'price': bitopro_response['data']['lastPrice']})
+
     update_sheet(service, SPREADSHEET_ID, update_data)
 
 
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
+        time.sleep(60)
